63a80e4645325d4456d1a85480f45899a9236060
jk2965 <jnh04215@gmail.com>
2022-10-06 19:09:30 +0900

    v2 commit

--- 2022-10-06/script.js
+++ 2022-10-06/script.js
@@ -1,1 +1,13 @@
-alert("hi");
+const player = {
+    name : "nico",
+    points : 1212121,
+    hansome : false,
+    fat : false
+
+};
+
+player.fat = true;
+
+player.lastName ="potato"
+console.log(player.name,player.points,
+    player.hansome,player.fat,player.lastName);
