0c7c02a5f5d31dbff4f27991e0b53770f001daaf
jk2965 <jnh04215@gmail.com>
2022-10-17 19:09:38 +0900

    2022/10/17 id저장하기

--- Momentum/script.js
+++ Momentum/script.js
@@ -1,10 +1,32 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting =document.querySelector("#greeting");
 
-console.dir(h1);
+const HIDDEN_CLASSNAME="hidden";
+const USERNAME_KEY="username";
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked");
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    loginForm.classList.add('hidden');
+    console.log(username);
+    paintGreetings();
 }
 
+function paintGreetings(){
+    const username=localStorage.getItem(USERNAME_KEY);
+    greeting.innerText=`Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-h1.onclick = handleTitleClick;
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+   paintGreetings();
+}
+
