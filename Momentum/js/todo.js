0aeda8dce46b11d774404c3b81ca052e2d9edb50
jk2965 <jnh04215@gmail.com>
2022-10-19 21:13:41 +0900

    2022/10/19 commit-2

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY="todos"
+
+const toDos=[];
+
+function saveToDos(){
+    localStorage.setItem("todos",JSON.stringify(toDos));
+    
+}
+
 function deleteToDo(event){
     const li=event.target.parentElement;
     li.remove();
@@ -23,7 +32,15 @@ function handleToDOSubmit(event){
     event.preventDefault();
     const newTODO = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTODO);
     paintTodo(newTODO);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDOSubmit);
+const savedToDos=localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(saveToDos);
+    parsedToDos.forEach();
+} 
